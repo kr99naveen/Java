a04f132039af3dbb48250be42eaee2fd0d1353c5
Naveen <pranav1111999@gmail.com>
2025-07-15 01:43:15 +0530

    REST api in spring first commit

---
+++ journalApp/src/main/java/com/naveen/journalApp/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.naveen.journalApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @RequestMapping("health")
+    public String healthCheck(){
+        return "OK";
+    }
+}
---
+++ journalApp/src/main/java/com/naveen/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,45 @@
+package com.naveen.journalApp.controller;
+
+import com.naveen.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        System.out.println("getting all journals");
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        System.out.println("creating journal");
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("{myId}")
+    public JournalEntry getJournalById(@PathVariable long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("{myId}")
+    public JournalEntry deleteJournalById(@PathVariable long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("{myId}")
+    public JournalEntry updateJournalById(@PathVariable long myId, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId, myEntry );
+    }
+
+}
