9aa133fb876aabfe2c595fe63cc82a0eba96bca3
Naveen <pranav1111999@gmail.com>
2025-07-17 01:21:04 +0530

    ResponseEntity and lombok project dependecy to reduce boilerplate codes in entities and classes

--- journalApp/src/main/java/com/naveen/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/naveen/journalApp/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.naveen.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +16,7 @@ import java.util.Date;
 
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     //unique key in entries, not manadatory, if not provided, db will create it
     @Id
@@ -20,36 +24,4 @@ public class JournalEntry {
     private  String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
